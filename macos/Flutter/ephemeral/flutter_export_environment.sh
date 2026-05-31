52bf737192d1fd607980cba09ee8814c42e0b270
GilangLabuhSamudra <glabuh96@gmail.com>
2026-05-31 10:07:28 +0700

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\1. KULIAH\1. Praktikum\1. PAB\Tugas\Praktikum\T1\praktikum1"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\1. KULIAH\1. Praktikum\1. PAB\Tugas\Praktikum\T1\praktikum1\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
